5e184e65b5ce67a9d8adfa408da2260b93a496be
pintuk300 <pintuk300@gmail.com>
2026-04-30 14:39:17 +0530

    Update code

--- src/shared/ui/TableCard.js
+++ src/shared/ui/TableCard.js
@@ -4,8 +4,9 @@ export const createTableCard = (table, isSelected, onClick) => {
     
     div.className = `table-card ${isSelected ? 'selected' : ''} ${isOccupied ? 'occupied' : ''}`;
     div.innerHTML = `
+        <span class="bullet" style="position:absolute; top:12px; right:12px; background:${isOccupied ? '#ef4444' : '#22c55e'};"></span>
         <div class="table-name">${table.name}</div>
-        <div class="table-status">${isOccupied ? 'Occupied' : 'Available'}</div>
+        <div class="table-status">${isOccupied ? 'In Use' : 'Ready'}</div>
     `;
 
     div.onclick = () => onClick(table.id);
